1c812234eaca1a20f6d6dc8c1149e6b1cd1cb9a7
Cem Sahin <55841039+j33z0n@users.noreply.github.com>
2023-01-18 22:29:55 +0100

    Edit pwdtools.py

--- pwdtools.py
+++ pwdtools.py
@@ -1,3 +1,5 @@
+import os
+
 class bcolors:
     HEADER = '\033[95m'
     OKBLUE = '\033[94m'
@@ -9,6 +11,11 @@ class bcolors:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
+def check_create_file(password_file):
+    if not os.path.exists(password_file):
+        with open(password_file, "wb") as fobj:
+            fobj.close()
+
 
 def linestripsplit(file, splitter):
     with open(file, "rb") as fobj:
@@ -17,7 +24,7 @@ def linestripsplit(file, splitter):
         content = fobj.read().decode("utf-8")
         lines = content.splitlines()
         while i < len(lines):
-            zuordnung = lines[i].split(splitter)
-            userpass[zuordnung[0]] = zuordnung[1][2:-1]
+            assignment = lines[i].split(splitter)
+            userpass[assignment[0]] = assignment[1][2:-1]
             i = i + 1
     return userpass
